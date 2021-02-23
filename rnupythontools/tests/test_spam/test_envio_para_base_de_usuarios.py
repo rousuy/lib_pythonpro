98076c83bd824c6af76e1dc80190678db71930c5
Rodrigo Usuy <rousuy@gmail.com>
2021-02-23 20:26:47 -0300

    configudo pipenv

--- rnupythontools/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ rnupythontools/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,7 +1,6 @@
 from unittest.mock import Mock
 
 import pytest
-from rnupythontools.spam.enviador_de_email import Enviador
 from rnupythontools.spam.main import EnviadordeSpam
 from rnupythontools.spam.modelos import Usuario
 
@@ -21,20 +20,40 @@ def test_qde_de_spam(sessao, usuarios):
     )
     assert len(usuarios) == enviador.enviar.call_count
 
-    def test_parametros_de_spam(sessao):
-        Usuario(nome='Rodrigo', email='rousuy@gmail.com')
-        sessao.salvar(usuarios)
-        enviador = Mock()
-        enviador_de_spam = EnviadordeSpam(sessao, enviador)
-        enviador_de_spam.enviar_emails(
-            'foo@bar.com.br',
-            'Curso de Python',
-            'Confira os módulos fantásticos'
-        )
-        assert enviador.enviar.assert_called_once_with == (
-            'foo@bar.com.br',
-            'rousuy@gmail.com',
-            'Curso de Python',
-            'Confira os módulos fantásticos'
-        )
+<<<<<<< HEAD
+=======
 
+def test_parametros_de_spam(sessao):
+    Usuario(nome='Rodrigo', email='rousuy@gmail.com')
+    sessao.salvar(usuarios)
+    enviador = Mock()
+    enviador_de_spam = EnviadordeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'foo@bar.com.br',
+        'Curso de Python',
+        'Confira os módulos fantásticos'
+    )
+    assert enviador.enviar.assert_called_once_with == (
+        'foo@bar.com.br',
+        'rousuy@gmail.com',
+        'Curso de Python',
+        'Confira os módulos fantásticos'
+    )
+>>>>>>> 44ef1139a57c7712bce1579dfe5e8e707b068a1d
+
+def test_parametros_de_spam(sessao):
+    Usuario(nome='Rodrigo', email='rousuy@gmail.com')
+    sessao.salvar(usuarios)
+    enviador = Mock()
+    enviador_de_spam = EnviadordeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'foo@bar.com.br',
+        'Curso de Python',
+        'Confira os módulos fantásticos'
+    )
+    assert enviador.enviar.assert_called_once_with == (
+        'foo@bar.com.br',
+        'rousuy@gmail.com',
+        'Curso de Python',
+        'Confira os módulos fantásticos'
+    )
